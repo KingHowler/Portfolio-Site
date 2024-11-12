85976d52904a899a65ba313ac03406efc42b60ce
M. Umar Shahbaz <shahbazu628@gmail.com>
2024-11-13 01:47:53 +0500

    Restructured website

--- assets/scripts/color-wheel.js
+++ assets/scripts/color-wheel.js
@@ -10,12 +10,28 @@ function changeBackgroundColor() {
   document.getElementById("mainPage").style.backgroundColor = `hsl(0, 100, 100)`;
   colorWheelItems = document.getElementsByClassName("colorWheel");
   LBcolorWheelItems = document.getElementsByClassName("lbcolorWheel");
+  RBcolorWheelItems = document.getElementsByClassName("rbcolorWheel");
+  TBcolorWheelItems = document.getElementsByClassName("tbcolorWheel");
+  BBcolorWheelItems = document.getElementsByClassName("bbcolorWheel");
+  BcolorWheelItems = document.getElementsByClassName("bcolorWheel");
   
   for(let i = 0; i < colorWheelItems.length; i++){
     colorWheelItems[i].style.color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
   }
   for(let i = 0; i < LBcolorWheelItems.length; i++){
     LBcolorWheelItems[i].style.borderLeft = `2px solid hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+  for(let i = 0; i < RBcolorWheelItems.length; i++){
+    RBcolorWheelItems[i].style.borderRight = `2px solid hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+  for(let i = 0; i < TBcolorWheelItems.length; i++){
+    TBcolorWheelItems[i].style.borderTop = `2px solid hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+  for(let i = 0; i < BBcolorWheelItems.length; i++){
+    BBcolorWheelItems[i].style.borderBottom = `2px solid hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+  for(let i = 0; i < BcolorWheelItems.length; i++){
+    BcolorWheelItems[i].style.border = `2px solid hsl(${hue}, ${saturation}%, ${lightness}%)`;
   }
 
   // Increment the hue for the next color; reset to 0 after reaching 360
