c9bd0b1a981680cf336a7afce8658f8e6d2ce2c7
M. Umar Shahbaz <shahbazu628@gmail.com>
2024-12-01 23:46:12 +0500

    Added nav buttons

--- assets/scripts/slideshow.js
+++ assets/scripts/slideshow.js
@@ -2,23 +2,44 @@ const images = document.querySelectorAll('.slide-image');
 const titleElement = document.getElementById('slide-title');
 const descriptionElement = document.getElementById('slide-description');
 
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let autoSlideTimer;
 let currentIndex = 0;
 
-function showNextSlide() {
+function showNextSlide(next) {
+  console.log(currentIndex + "  " + next);
   // Hide the current image
   images[currentIndex].classList.remove('active');
 
-  // Calculate the next image index
-  currentIndex = (currentIndex + 1) % images.length;
-
   // Show the next image
-  images[currentIndex].classList.add('active');
+  images[next].classList.add('active');
 
   // Update text overlay from data attributes
-  const currentSlide = images[currentIndex];
+  const currentSlide = images[next];
   titleElement.textContent = currentSlide.getAttribute('data-title');
   descriptionElement.textContent = currentSlide.getAttribute('data-description');
 }
 
+function autoSlide() {
+  showNextSlide((currentIndex + 1)  % images.length);
+  currentIndex = (currentIndex + 1) % images.length;
+}
+
 // Change images and text every 5 seconds
-setInterval(showNextSlide, 5000);
+autoSlideTimer = setInterval(autoSlide, 5000);
+
+prevBtn.addEventListener('click', () => {
+  showNextSlide((currentIndex - 1 + images.length) % images.length);
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  clearInterval(autoSlideTimer);
+  autoSlideTimer = setInterval(autoSlide, 5000);
+});
+
+nextBtn.addEventListener('click', () => {
+  showNextSlide((currentIndex + 1)  % images.length);
+  currentIndex = (currentIndex + 1) % images.length;
+  clearInterval(autoSlideTimer);
+  autoSlideTimer = setInterval(autoSlide, 5000);
+});
